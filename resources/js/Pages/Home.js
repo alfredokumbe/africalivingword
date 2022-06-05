143df2a1451dd9988d1b1ecbeab98cd071518b9b
mrmbeks <alfredokumbe1@gmail.com>
2022-06-05 12:33:35 +0300

    first commit

---
+++ resources/js/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <>
+            <div className="text-2xl container m-auto">Welcome Home Master</div>
+            <h1>This is the start of something greats</h1>
+        </>
+    );
+};
+
+export default Home;
